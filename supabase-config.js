2ba48188232e8dc0ce1028d469337b9f7b6470e5
infinity-wisdom <arimiewisdom@gmail.com>
2026-06-29 14:42:17 +0100

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -5,7 +5,7 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://lowyuuuexkqmctzwxyik.supabase.co';         // e.g. https://xyzcompany.supabase.co
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxvd3l1dXVleGtxbWN0end4eWlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTI1NTUsImV4cCI6MjA5ODIyODU1NX0.E0APjfeWJH1QEU2K9biiPwc5UX0xN7lZBmo9P7_2dFY'; // starts with eyJ...
+const SUPABASE_ANON_KEY = 'sb_publishable_N25vmBnSQwJzmOBfBAXCPw_rDvPzxoz'; // starts with eyJ...
 
 // Create the Supabase client (loaded via CDN in each HTML page)
 const { createClient } = supabase;
